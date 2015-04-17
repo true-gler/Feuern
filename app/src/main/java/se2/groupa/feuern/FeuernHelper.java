6c199bb476fb8aa6f3d261dac36a7f156d92e5e0
mplessnit <mplessnitzer@gmail.com>
2015-04-17 13:41:29 +0200

    FeuernHelper.class hinzugefügt (enthält static GameController-Objekt zum Datenaustausch zwischen Activitys)

---
+++ app/src/main/java/se2/groupa/feuern/FeuernHelper.java
@@ -0,0 +1,10 @@
+package se2.groupa.feuern;
+
+import se2.groupa.feuern.controller.GameController;
+
+/**
+ * Created by michaelplessnitzer on 17.04.15.
+ */
+public class FeuernHelper {
+    public static GameController gameController = null;
+}
